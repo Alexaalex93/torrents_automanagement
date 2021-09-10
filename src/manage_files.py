52488f2068631d2608d54c3052c716172c156e28
Alex <alexfdezglez93@gmail.com>
2021-09-10 16:55:33 +0200

    .

--- src/manage_files.py
+++ src/manage_files.py
@@ -8,7 +8,7 @@ Created on Thu Jul 29 10:12:38 2021
 import os
 
 def upload_to_backup_drive(**kwargs):
-    os.system(f'{kwargs["rclone_path"]} copy \"{kwargs["tmp_path"]}\" \"{kwargs["remote_name"]}:{kwargs["remote_folder"]}/{kwargs["category"]}\"')
+    os.system(f'{kwargs["rclone_path"]} copy \"{kwargs["source_path"]}\" \"{kwargs["remote_name"]}:{kwargs["remote_folder"]}/{kwargs["category"]}\"')
 
 def upload_to_drive(**kwargs):
     os.system(f'{kwargs["rclone_path"]} copy \"{kwargs["tmp_path"]}\" \"{kwargs["remote_name"]}:{kwargs["remote_folder"]}/{kwargs["folder_name"]}\"')
