a7771cf57d4d0fb6089b79495982e15a7eed9dde
Alex <alexfdezglez93@gmail.com>
2021-09-25 09:50:46 +0200

    ,

--- src/scrapper.py
+++ src/scrapper.py
@@ -82,6 +82,8 @@ def scrap_series(**kwargs):
     exports_folder = f'{kwargs["script_path"]}/utilities/tinyMediaManager/exports_{kwargs["file_name"].replace(" ", "_")}'
     #os.mkdir(exports_folder)
     os.system(f'{kwargs["script_path"]}/utilities/tinyMediaManager/tinyMediaManager tvshow -u --scrapeAll --renameAll -e -eT=tvshows_to_json -eP=\"{exports_folder}\"')
+    print(f'{exports_folder}/tvshows.json')
+    print(f'{kwargs["tmp_path"]}/{kwargs["file_name"]}')
     print(kwargs['file'])
     folder_name, resolution, plot, imdb_rating, imdb_id = get_series_folder(json_path=f'{exports_folder}/tvshows.json', tmp_file_path=f'{kwargs["tmp_path"]}/{kwargs["file_name"]}', file=kwargs['file'])
     os.system(f'{kwargs["script_path"]}/utilities/tinyMediaManager/tinyMediaManager tvshow --renameAll')
