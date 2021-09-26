0fe85657a4c73cad1a04839e186a956a42f7ce3b
Alex <alexfdezglez93@gmail.com>
2021-09-26 18:51:48 +0200

    ,

--- src/refactor_series.py
+++ src/refactor_series.py
@@ -19,7 +19,7 @@ def refactor_series(**kwargs):
         print(kwargs["file_name"])
         file = os.path.splitext(kwargs["file_name"])[0]
         print(file)
-        folder_name =  re.sub('(?i)s\d{1,2}e\d{1,2}.*\[.*\]', '', file).strip()
+        folder_name =  re.sub('(?i)s\d{1,2}e\d{1,2}(.*\[.*\])?', '', file).strip()
         print(folder_name)
         os.mkdir(f'{kwargs["tmp_path"]}/{folder_name}')
         shutil.move(f'{kwargs["tmp_path"]}/{kwargs["file_name"]}',  f'{kwargs["tmp_path"]}/{folder_name}')
