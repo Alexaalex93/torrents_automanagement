c795fd7d3170ccf4e9feed8ac6bf4edbae6f88de
Alex <alex.fernandez.0393@gmail.com>
2023-09-19 10:32:34 +0200

    changed destination drive

--- src/modules/manage_files.py
+++ src/modules/manage_files.py
@@ -10,6 +10,6 @@ import os
 def upload_to_drive(folder_to_upload_path, destination_folder_name, remote_name, logger):
 
 
-    logger.info( f'Rclone execution command: rclone copy \"{folder_to_upload_path}\" --ignore-existing \"{remote_name}:/{destination_folder_name}\"')
+    logger.info( f'Rclone execution command: rclone copy \"{folder_to_upload_path}\" --ignore-existing \"alexuc3m:/{remote_name}/{destination_folder_name}\"')
 
-    os.system(f'rclone copy \"{folder_to_upload_path}\" --ignore-existing \"{remote_name}:/{destination_folder_name}\"')
+    os.system(f'rclone copy \"{folder_to_upload_path}\" --ignore-existing \"alexuc3m:/{remote_name}/{destination_folder_name}\"')
